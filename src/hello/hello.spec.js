b93415ac9dcff63c4dd62cb109a71c26372669ec
Lee <leeblazek@gmail.com>
2017-06-06 22:29:42 -0600

    add hello spec

---
+++ src/hello/hello.spec.js
@@ -0,0 +1,30 @@
+import helloCtrl from './hello.ctrl';
+
+class sidenavServiceMock {
+  toggle(){
+    console.log('it toggled');
+  }
+}
+describe('hello', () => {
+
+  describe('helloCtrl', () => {
+    let ctrl;
+
+    beforeEach(() => {
+      // angular.mock.module(app);
+
+      // angular.mock.inject(($controller) => {
+      //   ctrl = $controller('AppCtrl', {});
+      // });
+      ctrl = new helloCtrl(new sidenavServiceMock);
+    });
+
+    it('should contain toggle sidebar', () => {
+      expect(ctrl.toggle).toExist;
+    });
+    it('should run toggle sidebar', () => {
+      ctrl.toggle();
+      // log
+    });
+  });
+});

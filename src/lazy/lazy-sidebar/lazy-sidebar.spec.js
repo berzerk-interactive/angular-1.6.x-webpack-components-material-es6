42b7301c68f6f16ac9f5c0131f92ccba2cfe94cb
Lee <leeblazek@gmail.com>
2017-06-07 21:48:06 -0600

    lazy sidebar spec

---
+++ src/lazy/lazy-sidebar/lazy-sidebar.spec.js
@@ -0,0 +1,25 @@
+import lazySidebarCtrl from './lazy-sidebar.ctrl';
+
+class sidenavServiceMock {
+  toggle(){
+    console.log('it toggled');
+  }
+}
+describe('lazy sidebar', () => {
+
+  describe('lazySidebarCtrl', () => {
+    let ctrl;
+
+    beforeEach(() => {
+      ctrl = new lazySidebarCtrl(new sidenavServiceMock);
+    });
+
+    it('should contain toggle sidebar', () => {
+      expect(ctrl.toggle).toExist;
+    });
+    it('should run toggle sidebar', () => {
+      ctrl.toggle();
+      // log
+    });
+  });
+});
